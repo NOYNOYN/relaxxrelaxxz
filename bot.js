63c2353c678932e0af7ff17094e51a6fabe65522
NOYNOYN <42329540+NOYNOYN@users.noreply.github.com>
2018-08-13 07:33:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -227,7 +227,7 @@ function isYoutube(str) {
     const embed = new Discord.RichEmbed()
      .setColor("RANDOM")
      .addField(`**__أوامر البوت__**`,`
-    ${prefix}play
+    **${prefix}play
    امر تشغيل الأغنية , !شغل الرابط او اسم الأعنية
     ${prefix}stop
         ايقاف الاغنية 
@@ -239,7 +239,7 @@ function isYoutube(str) {
       ايقاف الاغنية مؤقتا 
     ${prefix}resume
        امر تكملة الاغنية
-    ${prefix}move
+    ${prefix}move**
    سحب البوت او ادخال البوت الى روم
    
    صاحب البوت :<@452191687159185409>
